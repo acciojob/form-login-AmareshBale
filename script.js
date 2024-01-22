f8383cb14a3f4c7e6d4931bce9d671aac165286a
AmareshBale <137062501+AmareshBale@users.noreply.github.com>
2024-01-22 16:23:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+// script.js
 function getFormvalue() {
-    let firstName = document.querySelector('input[name="fname"]');
-    let lastName = document.querySelector('input[name="lname"]');
-    alert(`${firstName.value} ${lastName.value}`);
+  // Prevent the form from submitting in the traditional way
+  event.preventDefault()
+
+  // Get the values of First and Last Name
+  let firstName = document.querySelector('input[name="fname"]').value;
+  let lastName = document.querySelector('input[name="lname"]').value;
+
+  // Display the values using alert
+  alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
 }
 
-getFormvalue();
+// Attach the function to the form's submit event
+document.querySelector('form').addEventListener('submit', getFormvalue);
